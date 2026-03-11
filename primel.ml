5e3821d7f3910a53350da7e4fe7af9fa4b9f54b2
Felix <pnguyen72@my.bcit.ca>
2026-03-10 20:55:21 -0700

    refactor

--- primel.ml
+++ primel.ml
@@ -1,126 +1,166 @@
-type 'a lazystream = Stream of 'a * 'a lazystream Lazy.t
+(* ------ Helpers ------ *)
 
-let rec from n = Stream (n, lazy (from (n + 1)))
+let ( << ) = Fun.compose
 
-let rec take n (Stream (h, next)) =
-  if n <= 0 then [] else h :: take (n - 1) (Lazy.force next)
-;;
+(* Lazy stream, can be either finite or infinite *)
+module Stream = struct
+  type 'a t = Cons of 'a * 'a t Lazy.t | Empty
 
-let primes =
+  let first ~default = function Cons (item, _) -> item | _ -> default
+
+  let rec to_stream = function
+    | [] -> Empty
+    | x :: xs -> Cons (x, lazy (to_stream xs))
+
+  let singleton v = Cons (v, lazy Empty)
+
+  let rec count = function
+    | Cons (_, next) -> 1 + count (Lazy.force next)
+    | _ -> 0
+
+  let rec drop_while p = function
+    | Cons (item, next) when p item -> drop_while p (Lazy.force next)
+    | s -> s
+
+  let rec take_while p = function
+    | Cons (item, next) when p item ->
+        Cons (item, lazy (take_while p (Lazy.force next)))
+    | _ -> Empty
+
+  let rec map f = function
+    | Cons (item, next) -> Cons (f item, lazy (map f (Lazy.force next)))
+    | _ -> Empty
+
+  let rec iter f = function
+    | Cons (item, next) ->
+        f item;
+        iter f (Lazy.force next)
+    | _ -> ()
+
+  let rec filter p = function
+    | Cons (item, next) when p item ->
+        Cons (item, lazy (filter p (Lazy.force next)))
+    | Cons (_, next) -> filter p (Lazy.force next)
+    | _ -> Empty
+
+  let rec cat s1 s2 =
+    match s1 with
+    | Cons (item, next) -> Cons (item, lazy (cat (Lazy.force next) s2))
+    | _ -> s2
+
+  let rec lazy_cat s1 s2 =
+    match s1 with
+    | Cons (item, next) -> Cons (item, lazy (lazy_cat (Lazy.force next) s2))
+    | _ -> Lazy.force s2
+
+  let rec flatmap f = function
+    | Cons (item, next) ->
+        lazy_cat (f item) (lazy (flatmap f (Lazy.force next)))
+    | _ -> Empty
+end
+
+let rec to_digits = function 0 -> [] | n -> to_digits (n / 10) @ [ n mod 10 ]
+
+let rec starts_with = function
+  | x :: xs -> ( function y :: ys -> x = y && starts_with xs ys | [] -> false)
+  | [] -> fun _ -> true
+
+let rec matches idx v = function
+  | x :: xs when idx = 0 -> x = v
+  | _ :: xs -> matches (idx - 1) v xs
+  | [] -> false
+
+(* Because there are only 10 digits, using List is actually faster than Set *)
+let all_digits = List.init 10 Fun.id
+let remove v = List.filter (( <> ) v)
+
+let display_digits =
+  print_endline << String.concat " " << List.map string_of_int
+
+let display (p1, p2, p3) =
+  display_digits p1;
+  display_digits p2;
+  print_endline (String.make 9 '-');
+  display_digits p3;
+  print_newline ()
+
+(* ------ Main logic ------ *)
+
+(* Sieve of Eratosthenes *)
+let primes_generator =
   let module M = struct
     include Map.Make (Int)
 
     let safe_add n p map =
-      let rec handle_collision n = if mem n map then handle_collision (n + p) else n in
+      let rec handle_collision n =
+        if mem n map then handle_collision (n + p) else n
+      in
       add (handle_collision n) p map
-    ;;
-  end
-  in
+  end in
   let rec primes_from n map =
     match M.find_opt n map with
-    | None -> Stream (n, lazy (map |> M.add (n * n) n |> primes_from (n + 1)))
+    | None ->
+        Stream.Cons (n, lazy (map |> M.add (n * n) n |> primes_from (n + 1)))
     | Some p -> map |> M.remove n |> M.safe_add (n + p) p |> primes_from (n + 1)
   in
   primes_from 2 M.empty
-;;
 
-let rec drop_while p (Stream (h, next) as stream) =
-  if p h then drop_while p (Lazy.force next) else stream
-;;
+(* 5-digit primes, each represented as a list of digits *)
+let primes =
+  primes_generator
+  |> Stream.drop_while (fun p -> p < 10000)
+  |> Stream.take_while (fun p -> p <= 99999)
+  |> Stream.map to_digits
 
-let rec take_while p (Stream (h, next)) =
-  if p h then h :: take_while p (Lazy.force next) else []
-;;
-
-let product l = List.concat_map (fun e' -> l |> List.map (fun e -> e, e'))
-
-let rec to_digits =
-  let rec collect acc = function
-    | n when n = 0 -> acc
-    | n -> collect ((n mod 10) :: acc) (n / 10)
+(* Given p3, find all solutions (p1, p2, p3) *)
+let solve p3 =
+  (*
+    n = current digit index
+    p1s = remaining candidates for p1
+    p2s = remaining candidates for p2
+    digits = remaining digits (haven't been used by either p1 or p2)
+  *)
+  let rec aux (n, p1s, p2s, digits) =
+    let narrow = Stream.filter << matches n in
+    let extract_digits n = List.take n << Stream.first ~default:[] in
+    if
+      (* Removing invalid solutions *)
+      let p1 = extract_digits n p1s in
+      let p2 = extract_digits n p2s in
+      p1 > p2 || p1 = p3 || p2 = p3
+    then fun _ -> Stream.Empty
+    else function
+      | d :: ds when List.mem d digits ->
+          let digits = remove d digits in
+          (* For each digit of p3, either p1 or p2 matches it.
+            hit = remaining primes for the one that matches
+            misses = remaining primes for the other      
+          *)
+          let branch pair hits misses =
+            let hits = narrow d hits in
+            digits |> Stream.to_stream
+            |> Stream.flatmap (fun d' ->
+                let misses = narrow d' misses in
+                let digits = remove d' digits in
+                let p1s, p2s = pair hits misses in
+                aux (n + 1, p1s, p2s, digits) ds)
+          in
+          (* Generate solutions for both paths, then concatenate *)
+          Stream.cat
+            (branch (fun x y -> (x, y)) p1s p2s)
+            (branch (fun x y -> (y, x)) p2s p1s)
+      | [] -> (
+          match (p1s, p2s) with
+          | Stream.Cons (p1, _), Stream.Cons (p2, _) ->
+              Stream.singleton (p1, p2, p3)
+          | _ -> Stream.Empty)
+      (* When running out of digits *)
+      | _ :: _ -> Stream.Empty
   in
-  function
-  | n when n < 0 -> to_digits (-n)
-  | n when n = 0 -> [ 0 ]
-  | n -> collect [] n
-;;
+  aux (0, primes, primes, all_digits) p3
 
-let to_number =
-  let rec aux acc = function
-    | [] -> acc
-    | x :: xs -> aux ((acc * 10) + x) xs
-  in
-  aux 0
-;;
-
-(* Checks that a number, represented as list of digits, starts with the specified digits *)
-let rec starts_with = function
-  | x :: xs ->
-    (function
-      | y :: ys -> x = y && starts_with xs ys
-      | [] -> false)
-  | [] -> fun _ -> true
-;;
-
-let rec solve (acc_p1, acc_p2, p3, rem_p1, rem_p2, rem_digits) = function
-  | [] -> product rem_p1 rem_p2 |> List.map (fun (p1, p2) -> p1, p2, p3)
-  | d :: ds ->
-    if not (List.mem d rem_digits)
-    then []
-    else (
-      let p3 = p3 @ [ d ] in
-      match List.filter (( <> ) d) rem_digits with
-      | [] -> []
-      | rem_digits ->
-        (* If p1's digit matches *)
-        let solutions_1 =
-          let acc_p1 = acc_p1 @ [ d ] in
-          let rem_p1 = rem_p1 |> (acc_p1 |> starts_with |> List.filter) in
-          rem_digits
-          |> List.concat_map (fun d ->
-            let acc_p2 = acc_p2 @ [ d ] in
-            let rem_p2 = rem_p2 |> (acc_p2 |> starts_with |> List.filter) in
-            solve (acc_p1, acc_p2, p3, rem_p1, rem_p2, rem_digits) ds)
-        in
-        (* If p2's digit matches *)
-        let solutions_2 =
-          let acc_p2 = acc_p2 @ [ d ] in
-          let rem_p2 = rem_p2 |> (acc_p2 |> starts_with |> List.filter) in
-          rem_digits
-          |> List.concat_map (fun d ->
-            let acc_p1 = acc_p1 @ [ d ] in
-            let rem_p1 = rem_p1 |> (acc_p1 |> starts_with |> List.filter) in
-            solve (acc_p1, acc_p2, p3, rem_p2, rem_p1, rem_digits) ds)
-        in
-        solutions_1 @ solutions_2)
-;;
-
-let ( << ) = Fun.compose
-let print = print_endline << String.concat " " << List.map string_of_int
+let solutions = Stream.flatmap solve primes
 
 let () =
-  let solutions =
-    (* List of 5-digit primes, each is represented as a list of digits *)
-    let primes_digits =
-      primes
-      |> drop_while (fun p -> p < 10000)
-      |> take_while (fun p -> p <= 99999)
-      |> List.map to_digits
-    in
-    primes_digits
-    |> List.concat_map
-         (solve ([], [], [], primes_digits, primes_digits, from 0 |> take 10))
-    |> List.filter (fun (p1, p2, p3) ->
-      p3 <> p1 && p3 <> p2 && to_number p1 < to_number p2)
-  in
-  Printf.printf "%d solutions\n" (List.length solutions);
-  List.iter
-    (fun (p1, p2, p3) ->
-       print_newline ();
-       print p1;
-       print p2;
-       print_endline "---------";
-       print p3;
-       print_newline ())
-    solutions
-;;
+  Stream.iter display solutions;
+  Printf.printf "%d solutions\n" (Stream.count solutions)
