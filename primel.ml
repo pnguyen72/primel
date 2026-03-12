65cc3de8541b5329a5fe17206ca1fcdbbad6ef0c
Felix <pnguyen72@my.bcit.ca>
2026-03-11 19:54:28 -0700

    add progress bar

--- primel.ml
+++ primel.ml
@@ -2,6 +2,7 @@
 
 let ( << ) = Fun.compose
 let rec to_digits = function 0 -> [] | n -> to_digits (n / 10) @ [ n mod 10 ]
+let from_digits = List.fold_left (fun acc d -> (acc * 10) + d) 0
 
 let rec matches idx v = function
   | x :: _ when idx = 0 -> x = v
@@ -12,15 +13,34 @@ let rec matches idx v = function
 let all_digits = List.init 10 Fun.id
 let remove v = List.filter (( <> ) v)
 
-let display_digits =
-  print_endline << String.concat " " << List.map string_of_int
-
-let display (p1, p2, p3) =
-  display_digits p1;
-  display_digits p2;
-  print_endline (String.make 9 '-');
-  display_digits p3;
-  print_newline ()
+let progress_bar min max =
+  let fmin = float_of_int min and fmax = float_of_int max in
+  let last_percentage = ref (-1) in
+  let handle item =
+    let ratio = (float_of_int item -. fmin) /. (fmax -. fmin) in
+    let percentage = int_of_float (ratio *. 100.) in
+    if percentage <> !last_percentage then (
+      let bar ratio =
+        let completed = int_of_float (ratio *. 50.) in
+        let remaining = 50 - completed in
+        String.make completed '#' ^ String.make remaining ' '
+      in
+      Printf.printf "\r[%s] (%d%%)%!" (bar ratio) percentage;
+      last_percentage := percentage)
+  in
+  print_endline "Calculating...";
+  (* To show the bar immediately *)
+  handle min;
+  let rec aux = function
+    | Stream.Cons (item, next) ->
+        handle item;
+        aux (Lazy.force next)
+    | _ ->
+        (* To make the bar complete with 100% *)
+        handle max;
+        print_newline ()
+  in
+  aux
 
 (* ------ Main logic ------ *)
 
@@ -43,15 +63,16 @@ let primes_generator =
   in
   primes_from 2 M.empty
 
-(* 5-digit primes, each represented as a list of digits *)
 let primes =
   primes_generator
   |> Stream.drop_while (fun p -> p < 10000)
   |> Stream.take_while (fun p -> p <= 99999)
-  |> Stream.map to_digits
+
+let primes_digits = Stream.map to_digits primes
 
 (* Given p3, find all solutions (p1, p2, p3) *)
 let solve p3 =
+  let p3_digits = to_digits p3 in
   (*
     n = current digit index
     p1s = remaining candidates for p1
@@ -59,46 +80,60 @@ let solve p3 =
     digits = remaining digits (haven't been used by either p1 or p2)
   *)
   let rec aux (n, p1s, p2s, digits) =
-    let narrow = Stream.filter << matches n in
-    let extract_digits n = List.take n << Stream.first ~default:[] in
-    if
-      (* Removing invalid solutions *)
-      let p1 = extract_digits n p1s in
-      let p2 = extract_digits n p2s in
-      p1 > p2 || p1 = p3 || p2 = p3
-    then fun _ -> Stream.Empty
-    else function
-      | d :: ds when List.mem d digits ->
-          let digits = remove d digits in
-          (* For each digit of p3, either p1 or p2 matches it.
-            hit = remaining primes for the one that matches
+    if Stream.is_empty p1s || Stream.is_empty p2s then fun _ -> Stream.Empty
+    else
+      let extract_digits n = List.take n << Stream.first ~default:[] in
+      if
+        (* Invalid solutions *)
+        let p1_digits = extract_digits n p1s in
+        let p2_digits = extract_digits n p2s in
+        p1_digits > p2_digits || p1_digits = p3_digits || p2_digits = p3_digits
+      then fun _ -> Stream.Empty
+      else
+        let narrow = Stream.filter << matches n in
+        function
+        | d :: ds when List.mem d digits ->
+            let digits = remove d digits in
+            (* For each digit of p3, either p1 or p2 matches it.
+            hits = remaining primes for the one that matches
             misses = remaining primes for the other      
           *)
-          let branch pair hits misses =
-            let hits = narrow d hits in
-            digits |> Stream.to_stream
-            |> Stream.flatmap (fun d' ->
-                let misses = narrow d' misses in
-                let digits = remove d' digits in
-                let p1s, p2s = pair hits misses in
-                aux (n + 1, p1s, p2s, digits) ds)
-          in
-          (* Generate solutions for both paths, then concatenate *)
-          Stream.cat
-            (branch (fun x y -> (x, y)) p1s p2s)
-            (branch (fun x y -> (y, x)) p2s p1s)
-      | [] -> (
-          match (p1s, p2s) with
-          | Stream.Cons (p1, _), Stream.Cons (p2, _) ->
-              Stream.singleton (p1, p2, p3)
-          | _ -> Stream.Empty)
-      (* When running out of digits *)
-      | _ :: _ -> Stream.Empty
+            let branch pair hits misses =
+              let hits = narrow d hits in
+              digits |> Stream.to_stream
+              |> Stream.flatmap (fun d' ->
+                  let misses = narrow d' misses in
+                  let digits = remove d' digits in
+                  let p1s, p2s = pair hits misses in
+                  aux (n + 1, p1s, p2s, digits) ds)
+            in
+            (* Generate solutions for both cases, then concatenate *)
+            Stream.cat
+              (branch (fun x y -> (x, y)) p1s p2s)
+              (branch (fun x y -> (y, x)) p2s p1s)
+        | [] -> (
+            match (p1s, p2s) with
+            | Stream.Cons (p1_digits, _), Stream.Cons (p2_digits, _) ->
+                let p1 = from_digits p1_digits and p2 = from_digits p2_digits in
+                Stream.singleton (p1, p2, p3)
+            | _ -> Stream.Empty)
+        (* When running out of digits *)
+        | _ :: _ -> Stream.Empty
   in
-  aux (0, primes, primes, all_digits) p3
+  aux (0, primes_digits, primes_digits, all_digits) p3_digits
 
 let solutions = Stream.flatmap solve primes
 
 let () =
-  Stream.iter display solutions;
-  Printf.printf "%d solutions\n" (Stream.count solutions)
+  let out_file_name = "solutions.txt" in
+  let oc = open_out out_file_name in
+  solutions
+  |> Stream.map (fun (p1, p2, p3) ->
+      Printf.fprintf oc "%d, %d, %d\n" p1 p2 p3;
+      p3)
+  |> progress_bar
+       (primes |> Stream.first ~default:10000)
+       (primes |> Stream.last ~default:99999);
+  close_out oc;
+  Printf.printf "Solutions: %d\n" (Stream.count solutions);
+  Printf.printf "Full results written to %s\n" out_file_name
--- stream.mli
+++ stream.mli
@@ -1,13 +1,14 @@
 type 'a t = Cons of 'a * 'a t lazy_t | Empty
 
 val first : default:'a -> 'a t -> 'a
+val last : default:'a -> 'a t -> 'a
+val is_empty : 'a t -> bool
 val to_stream : 'a list -> 'a t
 val singleton : 'a -> 'a t
 val count : 'a t -> int
 val drop_while : ('a -> bool) -> 'a t -> 'a t
 val take_while : ('a -> bool) -> 'a t -> 'a t
 val map : ('a -> 'b) -> 'a t -> 'b t
-val iter : ('a -> unit) -> 'a t -> unit
 val filter : ('a -> bool) -> 'a t -> 'a t
 val cat : 'a t -> 'a t -> 'a t
 val flatmap : ('a -> 'b t) -> 'a t -> 'b t
